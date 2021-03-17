5bc5fbe01abc2bf2dceec4341cd8699e76de3006
jamera mcneil <j.mcneil04@gmail.com>
2021-03-17 14:50:03 -0400

    built out user_controller.js

--- gather_api/controllers/user_controller.js
+++ gather_api/controllers/user_controller.js
@@ -4,15 +4,25 @@ const users = express.Router()
 const User = require('../models/users.js');
 
 users.get('/signup', (req,res) => {
-    res.send('this is a user sign up')
+    User.find({}, (err, foundUser) => {
+		if(err) {
+			res.status(400).json({ error: err.message });
+		}
+		res.status(200).json(foundUser);
+	});
+    
 });
 
 users.post('/', (req, res) => {
     req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10))
-    User.create(req.body, (err, createdUser) => {
+    User.create(req.body, (error, createdUser) => {
+        if(error) {
+            res.status(400).json({ error: err.message})
+        }
+        res.status(200).send(createdUser)
         console.log('user is created', createdUser)
-        res.redirect('/')
-    })
-})
+        res.redirect('/events')
+    });
+});
 
 module.exports = users
