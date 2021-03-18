cdf84148904bee87f80f7753f18364f24970f1a2
jamera mcneil <j.mcneil04@gmail.com>
2021-03-18 14:19:12 -0400

    added method that returns a random number

--- gather_front_end/src/components/Explore.jsx
+++ gather_front_end/src/components/Explore.jsx
@@ -2,13 +2,13 @@ import axios from 'axios'
 import React, { Component } from 'react'
 import { Redirect } from 'react-router-dom'
 
-let baseURL = ''
+// let baseURL = ''
 
-if(process.env.NODE_ENV === 'development') {
-	baseURL = 'http://localhost:3003'
-} else {
-	baseURL = 'your heroku backend url here'
-}
+// if(process.env.NODE_ENV === 'development') {
+// 	baseURL = 'http://localhost:3003'
+// } else {
+// 	baseURL = 'your heroku backend url here'
+// }
 
 
 
@@ -16,31 +16,42 @@ export default class Explore extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            City: '',
-            State: '',
-            Activity: ''
+            cities: []
         }
     }
 
-    getCities() {
-        axios.get(baseURL + '/cities')
-        .then(data => {
-            console.log(data)
-            this.setState({ cities: data.data})
-        })
-    }
+    // getCities() {
+    //     axios.get(baseURL + '/cities')
+    //     .then(data => {
+    //         console.log(data)
+    //         this.setState({ cities: data.data})
+    //     })
+    // }
 
     // randomCities(cities) {
     //     let randomCity = cities[Math.floor(Math.random() * cities.length)];
     //     return randdomCity
     // }
 
+    componentDidMount() {
+        this.getRandom()
+    }
+
+    getRandom(min, max) {
+        min = Math.ceil(0)
+        max = Math.floor(14)
+        return( Math.floor(Math.random() * (max - min) + min) ) 
+    }
+    
     
     render() {
         return (
             <div>
                 <h3>Gather Here: </h3>
-                <h3>City: {this.state.city}</h3>
+                {/* <h3>City: {this.state.city}</h3> */}
+                <section>
+                    {/* {this.props.cities[this.getrandom()]} */}
+                </section>
             </div>
         )
     }
