5c5be29626578aa53633e50c9f69c8b19542bb3d
jamera mcneil <j.mcneil04@gmail.com>
2021-03-18 18:31:47 -0400

    successfully rendered cities data onto page

--- gather_front_end/src/components/EventCard.jsx
+++ gather_front_end/src/components/EventCard.jsx
@@ -62,7 +62,7 @@ export default class EventCard extends Component {
 						{ this.props.events.map(event => {
 							return (
 								<div className="each-item d-flex flex-column rounded position-relative" key={ event._id } >
-									<Link to={ event._id } className="router-link"><h5 className="each-title text-start">{ event.Title }</h5></Link>
+									<Link to={ "./" + event._id } className="router-link"><h5 className="each-title text-start">{ event.Title }</h5></Link>
 									<p className="each-name text-start"><strong>Added By:</strong> { event.Creator }</p>
 									<p className="each-name text-start"><strong>Date(s):</strong> { event.Date }</p>
                   {/* { this.findCategory(event => {
--- gather_front_end/src/components/Explore.jsx
+++ gather_front_end/src/components/Explore.jsx
@@ -2,24 +2,47 @@ import axios from 'axios'
 import React, { Component } from 'react'
 import { Redirect } from 'react-router-dom'
 
+let baseURL = ''
+
+if(process.env.NODE_ENV === 'development') {
+	baseURL = 'http://localhost:3003'
+} else {
+	baseURL = 'your heroku backend url here'
+}
+
 
 export default class Explore extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            someRandomCity: '',
+            cities: [],
+            someRandomCity: {
+                City: '',
+                State: '',
+                Activity: ''
+            },
         }
+    }
+
+    getCities() {
+        axios.get(baseURL + '/cities')
+        .then(data => {
+            console.log(data)
+            this.setState({ cities: data.data}, () => {
+                this.randomCities()
+            })
+        })
     }
 
 
     componentDidMount(){
-        this.randomCities(this.props.cities)
-        
+        this.getCities()
     }
 
-    randomCities(cities) {
-        console.log(cities)
-        let randomCity = cities[Math.floor(Math.random() * cities.length)];
+
+    randomCities() {
+
+        let randomCity = this.state.cities[Math.floor(Math.random() * this.state.cities.length)];
         console.log(randomCity)
         this.setState({
             someRandomCity: randomCity
@@ -33,6 +56,7 @@ export default class Explore extends Component {
             <div>
                 <h3>Gather Here: </h3>
                 <p>City: {this.state.someRandomCity.City} </p> 
+
                 <p>State: {this.state.someRandomCity.State} </p>
                 <p>Activity: {this.state.someRandomCity.Activity} </p>
 
